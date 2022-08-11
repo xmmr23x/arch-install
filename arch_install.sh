8a1dbc815e2448bbdcc4316742aadbbcf4287143
mmr23 <i72marom@uco.es>
2022-08-11 13:26:55 +0000

    corregido error chroot

--- arch_install.sh
+++ arch_install.sh
@@ -30,41 +30,41 @@ pacstrap /mnt linux-lts linux-firmware base base-devel wpa_supplicant networkman
 echo "Generando fstab para el sistema..."
 genfstab -U /mnt > /mnt/etc/fstab
 
-arch-chroot /mnt
+# arch-chroot /mnt
 
 # Contraseñas y usuarios
 
 echo "Estableciendo contraseña para el usuario root..."
-passwd
+arch-chroot /mnt passwd
 
 echo -n "Nombre del usuario: "
 read usuario
-useradd -m $usuario
+arch-chroot /mnt useradd -m $usuario
 
 echo "Estableciendo contraseña para el usuario $usuario..."
-passwd $usuario
+arch-chroot /mnt passwd $usuario
 
 echo "Creando usuario..."
-usermod -aG wheel $usuario
-sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' < /etc/sudoers > /etc/sudoers.b &
-mv /etc/sudoers.b /etc/sudoers &
+arch-chroot /mnt usermod -aG wheel $usuario
+sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' < /mnt/etc/sudoers > /mnt/etc/sudoers.b &
+mv /mnt/etc/sudoers.b /mnt/etc/sudoers &
 
 # idioma y archivos
 echo "Estableciendo idioma y generando archivos necesarios..."
-sed 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' < /etc/locale.gen > /etc/locale.gen.b &
+sed 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' < /mnt/etc/locale.gen > /mnt/etc/locale.gen.b &
 
-mv /etc/locale.gen.b /etc/locale.gen &
+mv /mnt/etc/locale.gen.b /mnt/etc/locale.gen &
 locale-gen &
 
-echo KEYMAP=es > /etc/vconsole.conf &
+echo KEYMAP=es > /mnt/etc/vconsole.conf &
 
 echo -n "Nombre del equipo: "
 read name
-echo $name > /etc/hostname
+echo $name > /mnt/etc/hostname
 
-echo "127.0.0.1\tlocalhost" >> /etc/hosts
-echo "::1\t\tlocalhost" >> /etc/hosts
-echo "127.0.0.1\t$name.localhost $name" >> /etc/hosts
+echo "127.0.0.1\tlocalhost" >> /mnt/etc/hosts
+echo "::1\t\tlocalhost" >> /mnt/etc/hosts
+echo "127.0.0.1\t$name.localhost $name" >> /mnt/etc/hosts
 
 # instalar grub
 
@@ -72,5 +72,5 @@ echo -n "Ruta para instalar el cargador de arranque: "
 read grub
 
 echo "Instalando el cargador de arranque"
-grub-install $grub &
-grub-mkconfig -o /boot/grub/grub.cfg &
+arch-chroot /mnt grub-install $grub &
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg &
